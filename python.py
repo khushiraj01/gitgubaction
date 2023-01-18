d09453f1b06247f7e3b717229ad4d7d3eedbf6c3
khushiraj01 <90633041+khushiraj01@users.noreply.github.com>
2023-01-18 22:12:01 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,10 +2,10 @@ print("hello world")
 
 
 def welcome():
+    print("welcome to devops batch")
   
-  
-  print("welcome to devops batch")
-  
-  
+   
 print("This is a github action tutorial")
-welcome()
+welcome()  
+  
+
